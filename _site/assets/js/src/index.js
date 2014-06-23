ebe02da289bc0025fec13bf39219a8b15d12aa7c
Micah Barrett <mbarrett02@risd.edu>
2014-06-23 18:45:33 -0400

    stylin' continues

--- _site/assets/js/src/index.js
+++ _site/assets/js/src/index.js
@@ -1,5 +1,13 @@
 // write JavaScript here
 
+$(window).scroll(function(){
+	if($(window).scrollTop()>10){
+		$('.main-nav').addClass('drawer');
+	} else if($(window).scrollTop()<20){
+		$('.main-nav').removeClass('drawer');
+	}
+});
+
 console.log($('p').text());
 
 $(window).load(function() {
