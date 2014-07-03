cae774db402d298194ea1e570eb9e0377bc28315
Micah Barrett <mbarrett02@risd.edu>
2014-07-03 09:24:28 -0400

    text updates

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,12 +1,12 @@
 // write JavaScript here
 
-$(window).scroll(function(){
-	if($(window).scrollTop()>10){
-		$('.main-nav').addClass('drawer');
-	} else if($(window).scrollTop()<20){
-		$('.main-nav').removeClass('drawer');
-	}
-});
+// $(window).scroll(function(){
+// 	if($(window).scrollTop()>10){
+// 		$('.main-nav').addClass('drawer');
+// 	} else if($(window).scrollTop()<20){
+// 		$('.main-nav').removeClass('drawer');
+// 	}
+// });
 
 console.log($('p').text());
 
