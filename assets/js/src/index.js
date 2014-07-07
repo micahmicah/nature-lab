fa7207dda152810e3ba9b171c5afaca5206af0a4
Micah Barrett <mbarrett02@risd.edu>
2014-07-07 16:30:11 -0400

    js stuff

---
+++ assets/js/src/index.js
@@ -0,0 +1,17 @@
+// write JavaScript here
+
+// $(window).scroll(function(){
+// 	if($(window).scrollTop()>10){
+// 		$('.main-nav').addClass('drawer');
+// 	} else if($(window).scrollTop()<20){
+// 		$('.main-nav').removeClass('drawer');
+// 	}
+// });
+
+console.log($('p').text());
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
