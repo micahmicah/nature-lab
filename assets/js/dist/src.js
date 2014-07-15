4b89c4cec6518107c718029850c5898f44e89ff1
rubillionaire <mail@rubenrodriguez.me>
2014-07-15 12:50:10 -0400

    lightbox + packery + draggabilly
    still need to sort out the lightbox closing when you click anywhere, but its getting there.

--- assets/js/dist/src.js
+++ assets/js/dist/src.js
@@ -9,65 +9,87 @@
 // 	}
 // });
 
+function setupFlexslider () {
+    if (!$('.flexslider').length) return;
 
-
-console.log($('p').text());
-
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-});
-
-$(function(){
-    $('.item').draggable();
-});
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('.main-nav').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.main-nav').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.main-nav').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
+    $('.flexslider').flexslider({
+      animation: "slide"
+    });
 }
 
+function setupHidingNavOnScroll () {
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.main-nav').outerHeight();
 
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
 
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-  // options
-  columnWidth: 200,
-  itemSelector: '.item'
-});
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('.main-nav').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('.main-nav').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+        
+        lastScrollTop = st;
+    }
+}
+
+function setupPackery () {
+    var container = document.querySelector('.packery-container');
+
+    if (!container) return;
+
+    var pckry = new Packery( container, {
+      // options
+      columnWidth: 200,
+      itemSelector: '.item'
+    });
+
+    pckry.getItemElements().forEach(function (item) {
+        var draggie = new Draggabilly(item);
+        pckry.bindDraggabillyEvents(draggie);
+    });
+}
+
+function NatureLab() {
+    var self = {};
+
+    // Set up all pages
+    self.initialize = function () {
+        setupHidingNavOnScroll();
+        setupFlexslider();
+        setupPackery();
+
+        return self;
+    };
+
+    return self;
+}
+
+var site = NatureLab().initialize();
+
 },{}]},{},[1])
